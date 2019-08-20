3b8357149f26b3db8ca764e4347db0531e80fc13
Dipayan Sen Gupta <dip.himu@gmail.com>
2019-08-20 13:31:17 +0600

    Add new model files with relation enabled

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'name','aisle'
+    ];
+    public function items(){
+        return $this->hasMany('App\Item');
+    }
 }
--- app/Item.php
+++ app/Item.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    protected $fillable = [
+
+        'name', 'price'
+
+    ];
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
